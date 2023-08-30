808e72249b0b90a2aec7a0684ecb60eabb20a183
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2023-08-30 15:54:47 +0000

    examen Joselyn Moncayo

--- main.cpp
+++ main.cpp
@@ -18,11 +18,10 @@ int main(int argc, char **argv) {
     float *b = new float[M];
     float *A = new float[M * M];
     float *c = new float[M];
-    int nfilas = M/nprocs;
-    int bloque = M*nfilas;
+
 
     if (rank == 0) {
-        M = 8;
+        M = 32;
 //Inicializar
         for (int i = 0; i < M * M; i++) {
             A[i] = i;
@@ -56,29 +55,58 @@ int main(int argc, char **argv) {
 
     if (rank == 0) {
 
-        /* M=8
+        /* M=32
          * nprocs=4
-         * nfilasxrank=2
-         * A[8x8]
+         * nfilas=8
+         * A[32*32]
          * rank0= A[0-7 y 8-15]=0-15
          * ran1= A[16-31] (nrank)*M*nfilas =16
          * rank2=A[32-47] (2*8*2)=32
          * rank3=A[48-63] (3*8*2)=48
          * bloque= M*nfilas
          */
+        int nfilas = M / nprocs;
+        int bloque = M * nfilas;
+        for (int nRank = 1; nRank < nprocs; nRank++) {
+            MPI_Send(&A[nRank * M * nfilas], bloque, MPI_FLOAT, nRank, 0, MPI_COMM_WORLD);
+        }
+
+        for (int j = 0; j < M * nfilas; j += M) {
+            float tmp = 0;
+            for (int i = 0; i < M; i++) {
+                tmp += A[i + j] * b[i];
+            }
+            c[j / M] = tmp;
+        }
 
         for (int nRank = 1; nRank < nprocs; nRank++) {
-            MPI_Send(&A[nRank*M*nfilas], bloque, MPI_FLOAT, nRank, 0, MPI_COMM_WORLD);
+            MPI_Recv(&c[nRank * nfilas], nfilas, MPI_FLOAT, nRank, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
+
+
+
+
     } else {
 
-        float *ATemp = new float [bloque];
-        MPI_Recv(&ATemp[0], AT, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int nfilas = M / nprocs;
+        int bloque = M * nfilas;
 
-        fmt::print("Recibiendo-rank: {}\n", rank);
-        for (int i = 0; i < bloque; i++) {
-            fmt::print("ATemp[{}]:{}\n", i, ATemp[i]);
+        float *ATemp = new float[bloque];
+        MPI_Recv(&ATemp[0], bloque, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        float *cTmp = new float[nfilas];
+
+        for (int j = 0; j < M * nfilas; j += M) {
+            float tmp = 0;
+            for (int i = 0; i < M; i++) {
+                tmp += ATemp[i + j] * b[i];
+            }
+            cTmp[j / M] = tmp;
         }
+
+        MPI_Send(&cTmp[0], nfilas, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+
+
     }
 
     MPI_Finalize();
